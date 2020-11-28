2a5c1f6550fda71efdf1cd66ee49e2eba4ee3104
Kristina Ilyovska <lyloster@gmail.com>
2020-11-28 12:34:19 -0800

    inspect edges

--- Assignments/3/bfs.c
+++ Assignments/3/bfs.c
@@ -36,81 +36,85 @@ void done(int* graph, Edge* path, int capacity, int sx, int sy, int tx, int ty);
 
 //should be moved in test?
 void find_path(int* graph, int sx, int sy, int tx, int ty) {
-  Queue* q;
-  q = (Queue*) malloc(sizeof(Queue));
-  //adding all the FREE outgoing edges from vertex S
-  if (sx + 1 < SIZE && graph[sx * SIZE * SIZE + sy * SIZE + 0] == FREE) {
-    Edge temp = {.start.x_coord = sx, .start.y_coord = sy,
-                  .target.x_coord = sx + 1, .target.y_coord = sy};
-    add(q, temp);
-    used_edge(sx, sy, sx + 1, sy);
-  }
-  if (sy + 1 < SIZE && graph[sx * SIZE * SIZE + sy * SIZE + 1] == FREE) {
-    Edge temp = {.start.x_coord = sx, .start.y_coord = sy,
-                  .target.x_coord = sx, .target.y_coord = sy + 1};
-    add(q, temp);
-    used_edge(sx, sy, sx, sy + 1);
-  }
-  if (sx - 1 > 0 && graph[sx * SIZE * SIZE + sy * SIZE + 2] == FREE) {
-    Edge temp = {.start.x_coord = sx, .start.y_coord = sy,
-                 .target.x_coord = sx - 1, .target.y_coord = sy};
-    add(q, temp);
-    used_edge(sx, sy, sx - 1, sy);
-  }
-  if (sy - 1 > 0 && graph[sx * SIZE * SIZE + sy * SIZE + 3] == FREE) {
-    Edge temp = {.start.x_coord = sx, .start.y_coord = sy,
-                  .target.x_coord = sx, .target.y_coord = sy - 1};
-    add(q, temp);
-    used_edge(sx, sy, sx, sy - 1);
-  }
-
-  Edge path [SIZE * SIZE];
-  int capacity = 0;
-  while (!isEmpty(q)) {
-    Edge temp = pop(q);;
-    path[capacity] = temp;
-    ++capacity;
-
-    if (temp.target.x_coord == tx && temp.target.y_coord == ty) {
-      done(graph, path, capacity, sx, sy, tx, ty);
-      break; //return?
-    }
-
-    if (temp.target.x_coord + 1 < SIZE && graph[temp.target.x_coord * SIZE * SIZE + temp.target.y_coord * SIZE + 0] == FREE) {
-      Edge temp_neighbor = {.start.x_coord = temp.target.x_coord,
-                            .start.y_coord = temp.target.y_coord,
-                            .target.x_coord = temp.target.x_coord + 1,
-                            .target.y_coord = temp.target.y_coord};
-      add(q, temp_neighbor);
-      used_edge(temp.target.x_coord, temp.target.y_coord, temp.target.x_coord, temp.target.y_coord + 1);
-    }
-
-    if (temp.target.y_coord + 1 < SIZE && graph[temp.target.x_coord * SIZE * SIZE + temp.target.y_coord * SIZE + 1] == FREE) {
-      Edge temp_neighbor = {.start.x_coord = temp.target.x_coord,
-                            .start.y_coord = temp.target.y_coord,
-                            .target.x_coord = temp.target.x_coord,
-                            .target.y_coord = temp.target.y_coord + 1};
-      add(q, temp_neighbor);
-      used_edge(temp.target.x_coord, temp.target.y_coord, temp.target.x_coord, temp.target.y_coord + 1);
-    }
-
-    if (temp.target.x_coord - 1 > 0 && graph[temp.target.x_coord * SIZE * SIZE + temp.target.y_coord * SIZE + 2] == FREE) {
-      Edge temp_neighbor = {.start.x_coord = temp.target.x_coord,
-                  .start.y_coord = temp.target.y_coord,
-                  .target.x_coord = temp.target.x_coord - 1,
-                  .target.y_coord = temp.target.y_coord};
-      add(q, temp_neighbor);
-      used_edge(temp.target.x_coord, temp.target.y_coord, temp.target.x_coord - 1, temp.target.y_coord);
-    }
-    if (temp.target.y_coord - 1 > 0 && graph[temp.target.x_coord * SIZE * SIZE + temp.target.y_coord * SIZE + 3] == FREE) {
-      Edge temp_neighbor = {.start.x_coord = temp.target.x_coord,
-                            .start.y_coord = temp.target.y_coord,
-                            .target.x_coord = temp.target.x_coord,
-                            .target.y_coord = temp.target.y_coord - 1};
-      add(q, temp_neighbor);
-      used_edge(temp.target.x_coord, temp.target.y_coord, temp.target.x_coord, temp.target.y_coord - 1);
-    }
-  }
+  printf("(1,1) to (1,2): %d'n", graph[1 * SIZE * SIZE + 1 * SIZE + 1]);
+  printf("(2,2) to (2,3): %d'n", graph[2 * SIZE * SIZE + 2 * SIZE + 1]);
+  printf("(2,3) to (2,4): %d'n", graph[2 * SIZE * SIZE + 3 * SIZE + 1]);
+  printf("(2,4) to (2,5): %d'n", graph[2 * SIZE * SIZE + 4 * SIZE + 1]);
+  // Queue* q;
+  // q = (Queue*) malloc(sizeof(Queue));
+  // //adding all the FREE outgoing edges from vertex S
+  // if (sx + 1 < SIZE && graph[sx * SIZE * SIZE + sy * SIZE + 0] == FREE) {
+  //   Edge temp = {.start.x_coord = sx, .start.y_coord = sy,
+  //                 .target.x_coord = sx + 1, .target.y_coord = sy};
+  //   add(q, temp);
+  //   used_edge(sx, sy, sx + 1, sy);
+  // }
+  // if (sy + 1 < SIZE && graph[sx * SIZE * SIZE + sy * SIZE + 1] == FREE) {
+  //   Edge temp = {.start.x_coord = sx, .start.y_coord = sy,
+  //                 .target.x_coord = sx, .target.y_coord = sy + 1};
+  //   add(q, temp);
+  //   used_edge(sx, sy, sx, sy + 1);
+  // }
+  // if (sx - 1 > 0 && graph[sx * SIZE * SIZE + sy * SIZE + 2] == FREE) {
+  //   Edge temp = {.start.x_coord = sx, .start.y_coord = sy,
+  //                .target.x_coord = sx - 1, .target.y_coord = sy};
+  //   add(q, temp);
+  //   used_edge(sx, sy, sx - 1, sy);
+  // }
+  // if (sy - 1 > 0 && graph[sx * SIZE * SIZE + sy * SIZE + 3] == FREE) {
+  //   Edge temp = {.start.x_coord = sx, .start.y_coord = sy,
+  //                 .target.x_coord = sx, .target.y_coord = sy - 1};
+  //   add(q, temp);
+  //   used_edge(sx, sy, sx, sy - 1);
+  // }
+  //
+  // Edge path [SIZE * SIZE];
+  // int capacity = 0;
+  // while (!isEmpty(q)) {
+  //   Edge temp = pop(q);;
+  //   path[capacity] = temp;
+  //   ++capacity;
+  //
+  //   if (temp.target.x_coord == tx && temp.target.y_coord == ty) {
+  //     done(graph, path, capacity, sx, sy, tx, ty);
+  //     break; //return?
+  //   }
+  //
+  //   if (temp.target.x_coord + 1 < SIZE && graph[temp.target.x_coord * SIZE * SIZE + temp.target.y_coord * SIZE + 0] == FREE) {
+  //     Edge temp_neighbor = {.start.x_coord = temp.target.x_coord,
+  //                           .start.y_coord = temp.target.y_coord,
+  //                           .target.x_coord = temp.target.x_coord + 1,
+  //                           .target.y_coord = temp.target.y_coord};
+  //     add(q, temp_neighbor);
+  //     used_edge(temp.target.x_coord, temp.target.y_coord, temp.target.x_coord, temp.target.y_coord + 1);
+  //   }
+  //
+  //   if (temp.target.y_coord + 1 < SIZE && graph[temp.target.x_coord * SIZE * SIZE + temp.target.y_coord * SIZE + 1] == FREE) {
+  //     Edge temp_neighbor = {.start.x_coord = temp.target.x_coord,
+  //                           .start.y_coord = temp.target.y_coord,
+  //                           .target.x_coord = temp.target.x_coord,
+  //                           .target.y_coord = temp.target.y_coord + 1};
+  //     add(q, temp_neighbor);
+  //     used_edge(temp.target.x_coord, temp.target.y_coord, temp.target.x_coord, temp.target.y_coord + 1);
+  //   }
+  //
+  //   if (temp.target.x_coord - 1 > 0 && graph[temp.target.x_coord * SIZE * SIZE + temp.target.y_coord * SIZE + 2] == FREE) {
+  //     Edge temp_neighbor = {.start.x_coord = temp.target.x_coord,
+  //                 .start.y_coord = temp.target.y_coord,
+  //                 .target.x_coord = temp.target.x_coord - 1,
+  //                 .target.y_coord = temp.target.y_coord};
+  //     add(q, temp_neighbor);
+  //     used_edge(temp.target.x_coord, temp.target.y_coord, temp.target.x_coord - 1, temp.target.y_coord);
+  //   }
+  //   if (temp.target.y_coord - 1 > 0 && graph[temp.target.x_coord * SIZE * SIZE + temp.target.y_coord * SIZE + 3] == FREE) {
+  //     Edge temp_neighbor = {.start.x_coord = temp.target.x_coord,
+  //                           .start.y_coord = temp.target.y_coord,
+  //                           .target.x_coord = temp.target.x_coord,
+  //                           .target.y_coord = temp.target.y_coord - 1};
+  //     add(q, temp_neighbor);
+  //     used_edge(temp.target.x_coord, temp.target.y_coord, temp.target.x_coord, temp.target.y_coord - 1);
+  //   }
+  // }
 }
 
 void done(int* graph, Edge* path, int capacity, int sx, int sy, int tx, int ty) {
@@ -175,53 +179,53 @@ Edge pop(Queue* list) {
   //}
 }
 
-// int inspect(Queue* list) {
-//   if (list == NULL) {
-//     printf("NULL POINTER");
-//     return - 1;
-//   } else if (list->head == NULL) {
-//     printf("EMPTY QUEUE");
-//     return -1;
-//   } else {
-//     return list->head->value;
-//   }
-// }
-//
-// int main() {
-//   Queue* start;
-//   start = (Queue*) malloc(sizeof(Queue));
-//   start->head = NULL;
-//   start->tail = NULL;;
-//   int empty = isEmpty(start);
-//   printf("empty == %d\n", empty);
-//   for (int i = 0; i < 10; ++i) {
-//     Edge e;
-//     e.start.x_coord = i;
-//     e.start.y_coord = i;
-//     e.target.x_coord = i + 1;
-//     e.target.y_coord = i + 1;
-//     add(start, e);
-//   }
-//   printf("Added edges\n");
-//   printQueue(start);
-//   printf("\n--------------------\n");
-//    Edge i = pop(start);
-//   // printf("Popped one edge\n");
-//   // printQueue(start);
-//   // printf("\n---------------------\n");
-//   //i = pop(start);
-//   printf("Popped one edge\n");
-//   printQueue(start);
-//   printf("\n---------------------\n");
-//   printf("The popped edge: (%d, %d), (%d,%d)\n", i.start.x_coord, i.start.y_coord, i.target.x_coord, i.target.y_coord);
-//   // for (int i = 0; i < 10; ++i) {
-//   //   Edge* i = pop(start);
-//   // }
-//   // i = inspect(start);
-//   // printf("i == %d\n", i);
-//   // printQueue(start);
-//   // printf("\n---------------------\n");
-//   empty = isEmpty(start);
-//   printf("empty == %d\n", empty);
-//   return 0;
-// }
+int inspect(Queue* list) {
+  if (list == NULL) {
+    printf("NULL POINTER");
+    return - 1;
+  } else if (list->head == NULL) {
+    printf("EMPTY QUEUE");
+    return -1;
+  } else {
+    return list->head->value;
+  }
+}
+
+int main() {
+  Queue* start;
+  start = (Queue*) malloc(sizeof(Queue));
+  start->head = NULL;
+  start->tail = NULL;;
+  int empty = isEmpty(start);
+  printf("empty == %d\n", empty);
+  for (int i = 0; i < 10; ++i) {
+    Edge e;
+    e.start.x_coord = i;
+    e.start.y_coord = i;
+    e.target.x_coord = i + 1;
+    e.target.y_coord = i + 1;
+    add(start, e);
+  }
+  printf("Added edges\n");
+  printQueue(start);
+  printf("\n--------------------\n");
+   Edge i = pop(start);
+  // printf("Popped one edge\n");
+  // printQueue(start);
+  // printf("\n---------------------\n");
+  //i = pop(start);
+  printf("Popped one edge\n");
+  printQueue(start);
+  printf("\n---------------------\n");
+  printf("The popped edge: (%d, %d), (%d,%d)\n", i.start.x_coord, i.start.y_coord, i.target.x_coord, i.target.y_coord);
+  // for (int i = 0; i < 10; ++i) {
+  //   Edge* i = pop(start);
+  // }
+  // i = inspect(start);
+  // printf("i == %d\n", i);
+  // printQueue(start);
+  // printf("\n---------------------\n");
+  empty = isEmpty(start);
+  printf("empty == %d\n", empty);
+  return 0;
+}
