6d57ce73968a8ddefe4b30b229b93ad0a6f386b5
Kristina Ilyovska <lyloster@gmail.com>
2020-12-05 15:13:34 -0800

    increase size of queue

--- Assignments/3/bfs.c
+++ Assignments/3/bfs.c
@@ -98,7 +98,7 @@ void find_path(int* graph, int sx, int sy, int tx, int ty) {
     printf("18\n");
   }
   printf("19\n");
-  Edge path [SIZE * SIZE * 4];
+  Edge path [SIZE * SIZE * SIZE * 4];
   printf("20\n");
   int capacity = 0;
   printf("21\n");
