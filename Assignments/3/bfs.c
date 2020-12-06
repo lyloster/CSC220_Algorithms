a2b37d6297d3d9cea184ec54e37c04467b54a9ce
Kristina Ilyovska <lyloster@gmail.com>
2020-12-05 18:37:22 -0800

    FINAL

--- Assignments/3/bfs.c
+++ Assignments/3/bfs.c
@@ -150,7 +150,6 @@ void find_path(int* graph, int sx, int sy, int tx, int ty) {
   while(1){
     path_edge(0,0,1,0); //hardcoded path to buffer used_edge green path
   }
-  exit(0);
 }
 
 //recreates the shortest path and colors it in orange
