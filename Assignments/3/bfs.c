062751d9e617b2679e9cf6f0513580b33fd0806e
Kristina Ilyovska <lyloster@gmail.com>
2020-12-05 17:52:06 -0800

    typos

--- Assignments/3/bfs.c
+++ Assignments/3/bfs.c
@@ -167,8 +167,8 @@ void done(int* graph, Edge* path, int capacity, int sx, int sy, int tx, int ty) 
   int targetY = ty;
   while (sx != targetX && sy != targetY) {
     for (int i = capacity - 1; i >= 0; --i) {
-      printf("%d/%d\n", i, capacity);
       if (path[i].target.x_coord == targetX && path[i].target.y_coord == targetY) {
+        printf("%d/%d\n", i, capacity);
         path_edge(path[i].start.x_coord, path[i].start.y_coord, path[i].target.x_coord, path[i].target.y_coord);
         targetX = path[i].start.x_coord;
         targetY = path[i].start.y_coord;
